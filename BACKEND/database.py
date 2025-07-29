9da97c94b786173f3d455f2c957948b6496f6041
Duvier Duarte <duvierduarte@cdrbolivar.edu.co>
2025-07-29 14:56:58 -0500

    cambie la  ruta de mi base de datos por la que me genero railway

--- BACKEND/database.py
+++ BACKEND/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 from sqlalchemy.exc import SQLAlchemyError
 
-DATABASE_URL = "mysql+pymysql://root@localhost:3306/app_db"
+DATABASE_URL = "mysql+pymysql://root:lMDyxrqOlWGIhknuPCIgwRTVPEeacJJI@ballast.proxy.rlwy.net:47753/railway"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
